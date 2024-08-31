9d2f68bb9f848f4e529a5b5a43d70ebf5e2a6cfc
Dindasela <dinda.120140078@student.itera.ac.id>
2024-09-01 02:06:24 +0700

    update input-field (admin)

--- resources/views/pages/admin/galeri/tambah-foto-video.blade.php
+++ resources/views/pages/admin/galeri/tambah-foto-video.blade.php
@@ -26,14 +26,6 @@
     <div class="pb-4 pl-4 pr-4 sm:ml-64">
         <h1 class="text-3xl font-bold pb-4">Tambah Foto dan Video</h1>
         <div class="flex pb-2">
-            {{-- <div class="">
-                <a href="/list-galeri" class="bg-[#2B2A4C] w-32 flex items-center p-1 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left">
-                        <path d="m12 19-7-7 7-7" />
-                        <path d="M19 12H5" /></svg>
-                    <span class="pl-3 font-bold flex-1 whitespace-nowrap">Kembali</span>
-                </a>
-            </div> --}}
         </div>
         <div class="relative overflow-x-auto">
             <!-- Form Section -->
@@ -44,9 +36,6 @@
                     <div class="flex flex-col items-center justify-center  border-gray-300 rounded-lg h-64 bg-[#D9D9D9]" id="dropzone">
                         <input id="fileInput" type="file" class="hidden" />
                         <div class="text-center" id="fileDisplayArea">
-                            {{-- <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mx-auto mb-2 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3 16s1-1 4-1 5 1 8 1 4-1 4-1v3a2 2 0 01-2 2H5a2 2 0 01-2-2v-3zM8 10h.01M16 10h.01M12 14h.01M21 12h-6.59l2.3-2.29a1 1 0 00-1.42-1.42l-4 4a1 1 0 000 1.42l4 4a1 1 0 001.42-1.42L14.41 14H21a1 1 0 000-2z" />
-                            </svg> --}}
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6    w-6 mx-auto mb-2 text-gray-400">
                                 <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="#4B4646" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                 <path d="M17 8L12 3L7 8" stroke="#4B4646" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -57,17 +46,25 @@
                         </div>
                     </div>
                     <!-- Form Fields -->
-                    <div class="space-y-4">
-                        <div>
-                            <input type="text" id="judul" name="judul" class="bg-[#F5F3F3] mt-1 block w-full px-3 py-2 border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Judul Foto atau Video">
+                    <form action="" method="">
+                        <div class="space-y-4 pb-4">
+                            <div>
+                                <input type="text" id="judul" name="judul" class="bg-[#F5F3F3] mt-1 block w-full px-3 py-2 border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Judul Foto atau Video">
+                            </div>
+                            <div>
+                                <select id="countries" class="bg-[#F5F3F3] mt-1 block w-full px-3 py-2 border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                    <option selected>Foto</option>
+                                    <option value="WNA">Video</option>
+                                </select>
+                            </div>
+                            <div>
+                                <input type="text" id="link" name="link" class="bg-[#F5F3F3] mt-1 block w-full px-3 py-2 border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Link Video">
+                            </div>
+                            <div>
+                                <textarea id="isi" name="isi" rows="4" class="bg-[#F5F3F3] mt-1 block w-full px-3 py-2 border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Deskripsi"></textarea>
+                            </div>
                         </div>
-                        <div>
-                            <input type="text" id="link" name="link" class="bg-[#F5F3F3] mt-1 block w-full px-3 py-2 border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Link Video">
-                        </div>
-                        <div>
-                            <textarea id="isi" name="isi" rows="4" class="bg-[#F5F3F3] mt-1 block w-full px-3 py-2 border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Deskripsi"></textarea>
-                        </div>
-                    </div>
+                    </form>
                 </div>
                 <div class="float-right text-center">
                     <button type="submit" class="bg-[#2B2A4C] text-white px-4 py-1 rounded-md hover:bg-[#414066]">Upload</button>
