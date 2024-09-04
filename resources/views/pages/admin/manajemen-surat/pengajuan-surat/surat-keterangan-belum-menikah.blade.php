99202a76f02ebefc11458e1adbd7e5b8352274c9
Dindasela <dinda.120140078@student.itera.ac.id>
2024-09-04 22:09:15 +0700

    update button acc & reject pengajuan-surat (admin)

--- resources/views/pages/admin/manajemen-surat/pengajuan-surat/surat-keterangan-belum-menikah.blade.php
+++ resources/views/pages/admin/manajemen-surat/pengajuan-surat/surat-keterangan-belum-menikah.blade.php
@@ -91,6 +91,10 @@
                             <img class="mx-auto h-full object-cover" src="image/preview.png" />
                         </div>
                     </div>
+                    <div class="flex justify-between mt-6 mx-auto w-[100%] pb-4">
+                        <button class="bg-[#2B2A4C] text-white px-6 py-1 rounded-md hover:bg-gray-400">Tolak</button>
+                        <button class="bg-[#D72323] text-white px-6 py-1 rounded-md hover:bg-red-700">Terima</button>
+                    </div>
                 </div>
             </div>
         </form>
