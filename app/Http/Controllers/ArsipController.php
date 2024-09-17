87527ed7be7cb1fb06da449b891cb8c2c9f71ca5
randaru09 <ndaruanggoro123@gmail.com>
2024-09-18 03:44:21 +0700

    feat : arsip

--- app/Http/Controllers/ArsipController.php
+++ app/Http/Controllers/ArsipController.php
@@ -24,54 +24,67 @@ class ArsipController extends Controller
         $sk_domisili = SuratKeluarDomisili::get();
         foreach ($sk_domisili as $key => $value) {
             $value->kategori = "Surat Keluar";
+            $value->jenis = "SKD";
         }
         $sk_kematian = SuratKeluarKematian::get();
         foreach ($sk_kematian as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SKK";
         }
         $sk = SuratKeluarKeterangan::get();
         foreach ($sk as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SK";
         }
         $sk_belum_menikah = SuratKeluarBelumMenikah::get();
         foreach ($sk_belum_menikah as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SKBM";
         }
         $sk_bersih_diri = SuratKeluarBersihDiri::get();
         foreach ($sk_bersih_diri as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SKBD";
         }
         $sk_domisili_usaha = SuratKeluarDomisiliUsaha::get();
         foreach ($sk_domisili_usaha as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SKDU";
         }
         $sk_kelahiran = SuratKeluarKelahiran::get();
         foreach ($sk_kelahiran as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SKKL";
         }
         $sk_penghasilan = SuratKeluarPenghasilan::get();
         foreach ($sk_penghasilan as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SKPOT";
         }
         $sk_pindah = SuratKeluarPindah::get();
         foreach ($sk_pindah as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SKP";
         }
         $sk_sudah_menikah = SuratKeluarSudahMenikah::get();
         foreach ($sk_sudah_menikah as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SKSM";
         }
         $sk_tidak_mampu = SuratKeluarTidakMampu::get();
         foreach ($sk_tidak_mampu as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SKTM";
         }
         $sk_usaha = SuratKeluarUsaha::get();
         foreach ($sk_usaha as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SKU";
         }
         $sk_pengantar_skck = SuratKeluarSKCK::get();
         foreach ($sk_pengantar_skck as $key => $value) {
             $value['kategori'] = "Surat Keluar";
+            $value->jenis = "SKPSKCK";
         }
         $sm = SuratMasuk::get();
         foreach ($sm as $key => $value) {
@@ -83,5 +96,68 @@ class ArsipController extends Controller
         }, collect());
 
         return view('pages.admin.manajemen-surat.arsip-surat.arsip-surat', compact('datas'));
+    }
+
+    public function lihatarsipsuratmasuk($id)
+    {
+        $data = SuratMasuk::find($id);
+        return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-masuk',compact('data'));
+    }
+
+    public function lihatarsipsuratkeluar($jenis, $id)
+    {
+        if ($jenis == "SKK") {
+            $data = SuratKeluarKematian::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SK") {
+            $data = SuratKeluarKeterangan::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SKBM") {
+            $data = SuratKeluarBelumMenikah::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SKBD") {
+            $data = SuratKeluarBersihDiri::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SKDU") {
+            $data = SuratKeluarDomisiliUsaha::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SKKL") {
+            $data = SuratKeluarKelahiran::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SKPOT") {
+            $data = SuratKeluarPenghasilan::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SKP") {
+            $data = SuratKeluarPindah::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SKSM") {
+            $data = SuratKeluarSudahMenikah::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SKTM") {
+            $data = SuratKeluarTidakMampu::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SKU") {
+            $data = SuratKeluarUsaha::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SKPSKCK") {
+            $data = SuratKeluarSKCK::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        } elseif ($jenis == "SKD") {
+            $data = SuratKeluarDomisili::find($id);
+            return view('pages/admin/manajemen-surat/arsip-surat/lihat-arsip-surat-keluar', compact(['data', 'jenis']));
+
+        }
     }
 }
